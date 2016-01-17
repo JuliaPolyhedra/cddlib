746ed721f1885e7177f18bc7858ebd306f0a34df
Benoît Legat <benoit.legat@gmail.com>
2016-01-17 17:55:03 +0100

    Added scripts to generate _f files by Matthias Troffaes

---
+++ make_all_f.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd lib-src-gmp
+
+../make_f.sh cddcore.c > cddcore_f.c
+../make_f.sh cddio.c > cddio_f.c
+../make_f.sh cddlib.c > cddlib_f.c
+../make_f.sh cddlp.c > cddlp_f.c
+../make_f.sh cddmp.c > cddmp_f.c
+../make_f.sh cddproj.c > cddproj_f.c
+
+../make_f.sh cdd.h > cdd_f.h
+../make_f.sh cddmp.h > cddmp_f.h
+../make_f.sh cddtypes.h > cddtypes_f.h
